eb7407f5878910db4f82f7f97189f5cf39bdf4fb
shaurCode <shaurya.rishi@yahoo.com>
2021-05-31 12:18:45 +0400

    Set up TypeScript

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </svelte:head>
 
-<script>
+<script lang="ts">
     import Navbar from "./Navbar.svelte";
 </script>
 
@@ -12,23 +12,5 @@
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,3 +1,11 @@
 <main>
-    <button class="btn btn-outline-primary">Test</button>
+    <div>
+        <h1>The world of</h1>
+    </div>
 </main>
+
+<style>
+    div {
+        border-bottom: solid #000000;
+    }
+</style>
