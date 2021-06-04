5c0e75506a5739f96d07ab8df5fd94b8c36fddbb
shaurCode <shaurya.rishi@yahoo.com>
2021-06-04 16:51:17 +0400

    Set up custom font for title

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,9 +1,32 @@
 <main>
     <!-- nav for navbar -->
-    <nav class="navbar navar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <!-- Full width container -->
         <div class="container-fluid">
-            
+           <a href="/" class="navbar-brand">
+                <p id="title-text-small">The world of</p>
+                <h1>Guitar</h1>
+           </a> 
         </div>
     </nav>
 </main>
+
+<style>
+    h1 {
+        /* Applies custom font family */
+        font-family: "Rawk Brush";
+        color: #00ffcc;
+        /* Gets rid of extra space at bottom of navbar */
+        margin-bottom: 0;
+    }
+    a:hover {
+        /* Get rid of odd-looking text decoration */
+        text-decoration: none;
+    }
+    #title-text-small {
+        font-family: "Rawk Brush";
+        font-size: 0.7em;
+        margin-bottom: 0;
+    }
+
+</style>
