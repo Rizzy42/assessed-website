5fbdba4574717d6f81674d99c06fb8e9ca9d40dd
shaurCode <shaurya.rishi@yahoo.com>
2021-07-02 11:51:49 +0400

    Finished bass page

--- src/pages/Bass.svelte
+++ src/pages/Bass.svelte
@@ -4,7 +4,17 @@
     const bassDesc1 = `
         The electric bass is the lowest-sounding (and likely the biggest) member of the guitar family.
         While it does look somewhat similar to its younger brother, don't be fooled. It generates a much
-        more different sound.
+        more interesting sound.
+    `
+    const bassDesc2 = `
+        Interestingly enough, the bass works on the same principle as the electric guitar: pickups convert
+        music vibrations into electric signals which are converted into sound by a loudspeaker. The reason
+        sound is so low is because of how thick the strings are, and how much longer the neck of an electric
+        bass is compared to the neck of a guitar.
+    `
+    const bassDesc3 = `
+        I could go on and on, but I prefer to let the sound do the talking. Continue listening to the above
+        video, and see the beauty of the bass for yourself!
     `
 </script>
 
@@ -18,13 +28,16 @@
         <div id="content">
             <h1>The electric bass</h1>
             <p>&nbsp;&nbsp;&nbsp;{bassDesc1}</p>
+            <iframe height="720" src="https://www.youtube.com/embed/FDKDdVilQT4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p id="desc-2">&nbsp;&nbsp;&nbsp;{bassDesc2}</p>
+            <p>&nbsp;&nbsp;&nbsp;{bassDesc3}</p>
             <div id="attrib">Photo by <a target="_blank" rel="noopener" class="link-attrib" href="https://unsplash.com/@almapapi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Krisztina Papp</a> on <a target="_blank" rel="noopener" class="link-attrib" href="https://unsplash.com/photos/KP5uGpi5ftQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></div>
         </div>
     </div>
 </template>
 
 <style lang="scss">
-    @mixin media-phone() {
+    @mixin media-phone($iframe-width) {
         #content {
             width: 80vw;
         }
@@ -61,6 +74,9 @@
             font-size: 1.5em;
         }
     }
+    #desc-2 {
+        margin-top: 1em;
+    }
     #attrib {
         text-align: center;
         margin-bottom: 2em;
@@ -72,13 +88,8 @@
             text-decoration: underline;
         }
     }
-    @media screen and (max-width:430px) {
-        @include media-phone();
-    }
-    @media screen and (min-width: 430px) and (max-width: 840px) {
-        @include media-phone();
-    }
-    @media screen and (min-width:840px) and (max-width:1390px) {
-        @include media-phone();
+    iframe {
+        width: 60vw;
+        height: 30vw;
     }
 </style>
